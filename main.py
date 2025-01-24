8c7690d0e19854b498ff44d5825c9821294badfe
attifbhuttoa <36535324+attifbhuttoa@users.noreply.github.com>
2025-01-24 16:08:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-import pandas as pd
-import glob
 from fpdf import FPDF
+import glob
 from pathlib import Path
 
-# create file path to access the excel files
-filepaths = glob.glob("invoices/*.xlsx")
+filepath = glob.glob("files/*.txt")
+pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-# iterrate over all the excel files
-for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-#     create pdf object
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
-    # add page
+
+for file in filepath:
     pdf.add_page()
-    # extract the file name
-    filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
-    # set-font
+    file_name = Path(file).stem
+    name = file_name.title()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, txt=f"Invoice number {invoice_nr}")
-    pdf.output(f"pdf/{filename}.pdf")
+    pdf.cell(w=50, txt=f"{name}", ln=1)
+    pdf.set_font(family="Times", size=10)
+
+    with open(file, "r") as text:
+        content = text.read()
+    pdf.set_font(family="Times", size=10)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
+
+
+
+pdf.output("all.pdf")
+
+
+
+
+
+
+
+
+
 
 
